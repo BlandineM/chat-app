0151dbb26f32a79af1342f324ddad181499fd2d6
blandine <blandine.mallart@gmail.com>
2019-10-17 08:53:41 +0200

    changement de la fonction en class + event sur le temoin

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,22 +3,32 @@ import './Style.css';
 
 
 
-function Contact (props){
-    return (
-        <div className="Contact"> 
-        
-            <img className ='avatar' src={props.avatar}></img>
-            <div className = "info">
-                <h3 className= 'name'>{props.name}</h3>
-                <div className = "status">
-                    <span className= {props.online? "status-online" : "status-offline"}></span>
-                    <p>{props.online? "On line" : "Off line"}</p>
+class Contact extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            online : props.online,
+        }
+    }
+    render(){
+        return (
+            <div className="Contact"> 
+            
+                <img className ='avatar' src={this.props.avatar}></img>
+                <div className = "info">
+                    <h3 className= 'name'>{this.props.name}</h3>
+                    <div className = "status">
+                        <span className= {this.state.online? "status-online" : "status-offline"}
+                        onClick= {event =>{
+                            const newOnline =! this.state.online;
+                            this.setState({online: newOnline});
+                        }}><p>{this.state.online? "Online" : "Offline"}</p>
+                        </span>
+                    </div>
                 </div>
             </div>
-       
-       
-        </div>
-    );
+        );
+    }
 }
 
 
